70936a2480b8d8b31849e634ec0e148f9c8a4779
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-04-21 15:49:09 -0700

    add specialty and office location functionality

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,10 +1,20 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 
-var displayDoctor = function (malady, doctorResults){
+var displayDoctor = function (malady, doctorResults, specialtyResults, practiceResults){
   $('#output').text("Here are the doctors in the Portland area that can help..." + "Your search word(s): " + "'" + malady + "'");
-  doctorResults.forEach(function(doctor){
-    $('.bios').append("<div class='row'><div class='col-md-12'><div class='well'><img alt='doctor_image' src=" + doctor.image_url + "><br><h4>Name: " + doctor.first_name + " " + doctor.last_name + ", " + doctor.title + "</h4> <h5>Gender: " + doctor.gender + "<div class='body'><h5>Bio: <br>" + doctor.bio + "</h5></div></div></div><br>");
-  });
+  for (var i = 0; i < doctorResults.length; i++){
+    $('.bios').append("<div class='row'><div class='col-md-12'><div class='well'><img alt='doctor_image' src=" + doctorResults[i].image_url + "><br><h4>Name: " + doctorResults[i].first_name + " " + doctorResults[i].last_name + ", " + doctorResults[i].title + "</h4> <h5>Gender: " + doctorResults[i].gender + "<br><div class='specialties" + i + "'>Specialties: </div><div class='practice" + i + "'>Office(s): </div><div class='body'><h5>Bio: <br>" + doctorResults[i].bio + "</h5></div></div></div><br>");
+  }
+  for (i = 0; i < specialtyResults.length; i++){
+    for (var j = 0; j < specialtyResults[i].length; j++){
+      $(".specialties" + i).append("<div class ='category'><ul><li> " + specialtyResults[i][j].actor + "</ul></li></div>");
+    }
+  }
+  for (i = 0; i < practiceResults.length; i++){
+    for (j = 0; j < practiceResults[i].length; j++){
+      $(".practice" + i).append("<div class='practice'><ul><li> " + practiceResults[i][j].visit_address.city + "</ul></li></div>" + practiceResults[i][j].visit_address.state_long + practiceResults[i][j].visit_address.street + practiceResults[i][j].visit_address.zip);
+    }
+  }
 };
 
 $(document).ready(function(){
