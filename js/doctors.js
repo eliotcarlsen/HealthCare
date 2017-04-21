2c1a7a4904f2bd141f41c2816e2523e321eefe2b
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-04-21 13:43:42 -0700

    add a working site that can pull bios from doctors and display them

--- js/doctors.js
+++ js/doctors.js
@@ -5,15 +5,17 @@ function Doctor(){
 
 Doctor.prototype.getDoctors = function(malady, displayDoctors){
   doctorArray = [];
+  hospitalArray = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + malady +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
+     console.log(result.data);
      result.data.forEach(function(doctor){
        doctorArray.push(doctor.profile);
      });
      console.log(doctorArray);
      displayDoctors(malady, doctorArray);
     }).fail(function(error){
-    $('#output').text(error.responseJSON.message);
+    $('.bios').text(error.responseJSON.message);
   });
 };
 
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -1,12 +1,16 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 
 var displayDoctor = function (malady, doctorResults){
-  $('#output').text("Here are the doctors in your area that can treat " + malady + doctorResults);
+  $('#output').text("Here are the doctors in the Portland area that can help..." + "Your search word(s): " + "'" + malady + "'");
+  doctorResults.forEach(function(doctor){
+    $('.bios').append("<div class='row'><div class='col-md-12'><div class='well'><img alt='doctor_image' src=" + doctor.image_url + "><br><h4>Name: " + doctor.first_name + " " + doctor.last_name + ", " + doctor.title + "</h4> <h5>Gender: " + doctor.gender + "<div class='body'><h5>Bio: <br>" + doctor.bio + "</h5></div></div></div><br>");
+  });
 };
 
 $(document).ready(function(){
   $('form#input').submit(function(event){
     event.preventDefault();
+    $('.bios').empty();
     var malady = $('#malady').val();
     var doctor = new Doctor();
     doctor.getDoctors(malady, displayDoctor);
