4d94b95a76ae50f78f34c4ed217505bb078f8090
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-04-21 18:37:28 -0700

    removed console.log

--- js/doctors.js
+++ js/doctors.js
@@ -10,15 +10,11 @@ Doctor.prototype.getDoctors = function(malady, displayDoctors){
   practiceArray = [];
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + malady +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     console.log(result.data);
      result.data.forEach(function(doctor){
        doctorArray.push(doctor.profile);
        specialtyArray.push(doctor.specialties);
        practiceArray.push(doctor.practices);
      });
-     console.log(doctorArray);
-     console.log(specialtyArray);
-     console.log(practiceArray);
      displayDoctors(malady, doctorArray, specialtyArray, practiceArray);
     }).fail(function(error){
     $('.bios').text(error.responseJSON.message);
