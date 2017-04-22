89722c89299e5549bd96e4c165ecbd29a412f3f1
Eliot Carlsen <eliot.carlsen@gmail.com>
2017-04-21 17:46:53 -0700

    add styling for profiles

--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,18 +1,18 @@
 var Doctor = require('./../js/doctors.js').doctorModule;
 
 var displayDoctor = function (malady, doctorResults, specialtyResults, practiceResults){
-  $('#output').text("Here are the doctors in the Portland area that can help..." + "Your search word(s): " + "'" + malady + "'");
+  $('#output').text("Here are the Portland doctors that can help..." + "Your search word(s): " + "'" + malady + "'");
   for (var i = 0; i < doctorResults.length; i++){
-    $('.bios').append("<div class='row'><div class='col-md-12'><div class='well'><img alt='doctor_image' src=" + doctorResults[i].image_url + "><br><h4>Name: " + doctorResults[i].first_name + " " + doctorResults[i].last_name + ", " + doctorResults[i].title + "</h4> <h5>Gender: " + doctorResults[i].gender + "<br><div class='specialties" + i + "'>Specialties: </div><div class='practice" + i + "'>Office(s): </div><div class='body'><h5>Bio: <br>" + doctorResults[i].bio + "</h5></div></div></div><br>");
+    $('.bios').append("<div class='container'><div class='well'><div class='row'><div class='col-md-12'><div class='col-md-4'><img alt='doctor_image' src=" + doctorResults[i].image_url + "><br><h4>Name: " + doctorResults[i].first_name + " " + doctorResults[i].last_name + ", " + doctorResults[i].title + "</h4> <h5>Gender: " + doctorResults[i].gender + "</h5></div><div class='col-md-4'><div class='specialties'><div class='specialties" + i + "'>Specialties: </div></div></div><div class='col-md-4'><div class='practice'><div class='practice" + i + "'>Office(s): </div></div></div><div class='col-md-12'><div class='body'>Bio: <br>" + doctorResults[i].bio + "</h5></div></div></div></div></div></div>");
   }
   for (i = 0; i < specialtyResults.length; i++){
     for (var j = 0; j < specialtyResults[i].length; j++){
-      $(".specialties" + i).append("<div class ='category'><ul><li> " + specialtyResults[i][j].actor + "</ul></li></div>");
+      $(".specialties" + i).append("<div class ='categories'><ul><li> " + specialtyResults[i][j].actor + "</li></ul></div>");
     }
   }
   for (i = 0; i < practiceResults.length; i++){
     for (j = 0; j < practiceResults[i].length; j++){
-      $(".practice" + i).append("<div class='practice'><ul><li> " + practiceResults[i][j].visit_address.city + "</ul></li></div>" + practiceResults[i][j].visit_address.state_long + practiceResults[i][j].visit_address.street + practiceResults[i][j].visit_address.zip);
+      $(".practice" + i).append("<div class='practiceOffices'><ul><li> " + practiceResults[i][j].visit_address.city + ", " + practiceResults[i][j].visit_address.state_long + "</li></ul><ul><li>" + practiceResults[i][j].visit_address.street + ", " + practiceResults[i][j].visit_address.zip + "</ul></li></div>");
     }
   }
 };
